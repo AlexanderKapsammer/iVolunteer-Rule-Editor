7cebf1f10ed3837b60de0ff3e0b5ce1fe0909743
Alex <kapsammeralex@gmail.com>
2019-07-29 15:47:22 +0200

    added drool generation

--- rule_editor_prototype/src/App.js
+++ rule_editor_prototype/src/App.js
@@ -3,217 +3,145 @@ import RuleName from "./formElements/RuleName";
 import Conditions from "./formElements/Conditions";
 import RequiredBadges from "./formElements/RequiredBadges";
 
-class App extends React.Component
-{
-    constructor()
-    {
-        super();
-        this.state = {
-            existingBadges: [],
-            ruleName: "",
-            ruleConditions: [
-                {
-                    conditionType: "required",
-                    conditionCount: 1,
-                    conditionObject: ""
-                }
-            ],
-            ruleRequiredBadges: [
-                {
-                    requiredType: "required",
-                    requiredObject: ""
-                }
-            ]
-        };
-
-        // binding this. for this.setState() to work in those functions
-        this.handleChange = this.handleChange.bind(this);
-        this.handleAddCondition = this.handleAddCondition.bind(this);
-        this.handleRemoveCondition = this.handleRemoveCondition.bind(this);
-        this.handleConditionsChange = this.handleConditionsChange.bind(this);
-        this.handleAddBadge = this.handleAddBadge.bind(this);
-        this.handleRemoveBadge = this.handleRemoveBadge.bind(this);
-        this.handleBadgesChange = this.handleBadgesChange.bind(this);
-        this.createRule = this.createRule.bind(this);
-    }
-    componentDidMount()
-    {
-        // get the data (e.g. existing badge names) from the server
-
-        // filling existingBadges with demo data
-        const newExistingBadges=[
-            "Rettungssanitäter",
-            "Rettungsfahrzeuglenker",
-            "Feuerwehrgruppenkommandant",
-            "much more stuff"
-        ]
-        this.setState({existingBadges: newExistingBadges});
-    }
-
-    // ==============================================================
-
-    // handle events in conditon lines
-    handleAddCondition()
-    {
-        let newRuleConditions = this.state.ruleConditions.slice();
-        newRuleConditions.push( {
-            conditionType: "required",
-            conditionCount: 1,
-            conditionObject: ""
-        } );
-        this.setState({ruleConditions: newRuleConditions})
-    }
-    handleRemoveCondition(event)
-    {
-        const conditionindex = event.target.getAttribute("conditionindex");
-        let newRuleConditions = this.state.ruleConditions.slice();
-        newRuleConditions.splice(conditionindex, 1);
-        this.setState({ruleConditions: newRuleConditions});
-    }
-    handleConditionsChange(event)
-    {
-        const{name, value} = event.target;
-        const conditionindex = event.target.getAttribute("conditionindex");
-        let newRuleConditions = this.state.ruleConditions.slice();
-        if(name === "conditionCount" && value < 1)
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      ruleName: "",
+      ruleConditions: [
         {
-            alert("The count must be at least 1!");
-            newRuleConditions[conditionindex] = {
-                ...newRuleConditions[conditionindex],
-                conditionCount: 1
-            }
-        }
-        else
+          conditionType: "",
+          conditionCount: 1,
+          conditionObject: ""
+        },
         {
-            newRuleConditions[conditionindex] = {
-                ...newRuleConditions[conditionindex],
-                [name]: value
-            }
+          conditionType: "",
+          conditionCount: 1,
+          conditionObject: "hallo"
         }
-        this.setState({ruleConditions: newRuleConditions});
+      ]
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAddCondition = this.handleAddCondition.bind(this);
+    this.handleRemoveCondition = this.handleRemoveCondition.bind(this);
+    this.handleConditionsChange = this.handleConditionsChange.bind(this);
+    this.createRule = this.createRule.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState(() => {
+      return { [name]: value };
+    });
+  }
+
+  handleAddCondition() {
+    let newRuleConditions = this.state.ruleConditions.slice();
+    newRuleConditions.push({
+      conditionType: "",
+      conditionCount: 1,
+      conditionObject: ""
+    });
+    this.setState({ ruleConditions: newRuleConditions });
+  }
+
+  handleRemoveCondition(event) {
+    const conditionindex = event.target.getAttribute("conditionindex");
+    let newRuleConditions = this.state.ruleConditions.slice();
+    newRuleConditions.splice(conditionindex, 1);
+    this.setState({ ruleConditions: newRuleConditions });
+  }
+
+  handleConditionsChange(event) {
+    const { name, value } = event.target;
+    const conditionindex = event.target.getAttribute("conditionindex");
+    let newRuleConditions = this.state.ruleConditions.slice();
+    if (name === "conditionCount" && value < 1) {
+      alert("The count must be at least 1!");
+      newRuleConditions[conditionindex] = {
+        ...newRuleConditions[conditionindex],
+        conditionCount: 1
+      };
+    } else {
+      newRuleConditions[conditionindex] = {
+        ...newRuleConditions[conditionindex],
+        [name]: value
+      };
+    }
+    this.setState({ ruleConditions: newRuleConditions });
+  }
+
+  createRule() {
+    for (let i = 0; i < this.state.ruleConditions.length; i++) {
+      if (
+        this.state.ruleConditions[i].conditionType === "" ||
+        this.state.ruleConditions[i].conditionObject === "" ||
+        this.state.ruleName === ""
+      ) {
+        alert("Complete filling in the form!");
+        return;
+      }
     }
 
-    // ===============================================================
-
-    // handle events in badge lines
-    // very similar to condition lines
-    handleAddBadge()
-    {
-        let newRuleRequiredBadges = this.state.ruleRequiredBadges.slice();
-        newRuleRequiredBadges.push({
-            requiredType: "required",
-            requiredObject: ""
-        });
-        this.setState({ruleRequiredBadges: newRuleRequiredBadges});
+    // create drools file and export it
+    let drool = 'dialect  "mvel"\r\nimport model.Volunteer \r\n';
+    drool +=
+      'rule "' + this.state.ruleName + '" when volunteer: model.Volunteer(';
+    for (let i = 0; i < this.state.ruleConditions.length; i++) {
+      if (i !== 0) {
+        drool += "&& ";
+      }
+      drool +=
+        this.state.ruleConditions[i].conditionObject +
+        " == " +
+        this.state.ruleConditions[i].conditionCount;
     }
-    handleRemoveBadge(event)
-    {
-        console.log("got till here!");
-        
-        const badgeindex = event.target.getAttribute("badgeindex");
-        let newRuleRequiredBadges = this.state.ruleRequiredBadges.slice();
-        newRuleRequiredBadges.splice(badgeindex, 1);
-        this.setState({ruleRequiredBadges: newRuleRequiredBadges});
+    drool += ') then System.out.println("' + this.state.ruleName + '"); end';
+
+    const element = document.createElement("a");
+    const file = new Blob([drool], {
+      type: "text/plain"
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = "rules.drl";
+    document.body.appendChild(element);
+    element.click();
+
+    // just for displaying data
+    let message = "New rule name: " + this.state.ruleName + "\n\n";
+    for (let i = 0; i < this.state.ruleConditions.length; i++) {
+      message +=
+        " " +
+        (i + 1).toString() +
+        ".  " +
+        this.state.ruleConditions[i].conditionType +
+        ":   " +
+        this.state.ruleConditions[i].conditionCount +
+        " " +
+        this.state.ruleConditions[i].conditionObject +
+        "\n";
     }
-    handleBadgesChange(event)
-    {
-        const{name, value} = event.target;
-        const badgeindex = event.target.getAttribute("badgeindex");
-        let newRuleRequiredBadges = this.state.ruleRequiredBadges.slice();
-        newRuleRequiredBadges[badgeindex] = {
-            ...newRuleRequiredBadges[badgeindex],
-            [name]: value
-        }
-        this.setState({ruleRequiredBadges: newRuleRequiredBadges});
-    }
+    alert(message);
+  }
 
-    // ===============================================================
-
-    // handle events in App
-    handleChange(event)
-    {
-        const{name, value} = event.target;
-        this.setState(() => { return( {[name]: value} ); });
-    }
-    createRule()
-    {
-        if(!(this.state.ruleConditions.length > 0 || this.state.ruleRequiredBadges.length > 0) || this.state.ruleName == "")
-        {
-            alert("Complete filling in the form!");
-            return;                
-        }
-        for(let i = 0; i < this.state.ruleConditions.length; i++)
-        {
-            if(this.state.ruleConditions[i].conditionObject === "")
-            {
-                alert("Complete filling in the form!");
-                return;
-            }
-        }
-        for(let i = 0; i < this.state.ruleRequiredBadges.length; i++)
-        {
-            if(this.state.ruleRequiredBadges[i].requiredObject === "")
-            {
-                alert("Complete filling in the form!");
-                return;
-            }
-        }
-
-        // check if chosen rule name already exists, if so,
-        // reject request and send alert
-        // create drools file and export it
-        // all the right data sits in this.state!
-
-        // just for displaying data
-        let message = "New rule name: " + this.state.ruleName + "\n\nAchievement(s):\n";
-        for(let i = 0; i < this.state.ruleConditions.length; i++)
-        {
-            message += " " + 
-            (i + 1).toString() + ".  " + 
-            this.state.ruleConditions[i].conditionType + ":   " + 
-            this.state.ruleConditions[i].conditionCount + " " + 
-            this.state.ruleConditions[i].conditionObject + "\n";
-        }
-        message += "\nBadge(s):\n";
-        for(let i = 0; i < this.state.ruleRequiredBadges.length; i++)
-        {
-            message += " " + 
-            (i + 1).toString() + ".  " + 
-            this.state.ruleRequiredBadges[i].requiredType + ":   " + 
-            this.state.ruleRequiredBadges[i].requiredObject + "\n";
-        }
-        alert(message);
-    }
-
-    // ======================================================================
-
-    render()
-    {
-        return(
-            <div>
-                <h1>Ivolunteer rule editor prototype</h1>
-                <hr />
-                <RuleName value={this.state.ruleName} onChange={this.handleChange}/>
-                <hr />
-                <Conditions 
-                    onAdd={this.handleAddCondition} 
-                    ruleConditions={this.state.ruleConditions}
-                    onConditionsChange={this.handleConditionsChange}
-                    onSingleRemove={this.handleRemoveCondition}
-                />
-                <hr />
-                <RequiredBadges 
-                    onAdd={this.handleAddBadge}
-                    ruleRequiredBadges={this.state.ruleRequiredBadges}
-                    existingBadges={this.state.existingBadges}
-                    onBadgesChange={this.handleBadgesChange}
-                    onSingleRemove={this.handleRemoveBadge}
-                />
-                <hr />
-                <button onClick={this.createRule}>create rule</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>iVolunteer rule editor prototype</h1>
+        <hr />
+        <RuleName value={this.state.ruleName} onChange={this.handleChange} />
+        <hr />
+        <Conditions
+          onAdd={this.handleAddCondition}
+          ruleConditions={this.state.ruleConditions}
+          onConditionsChange={this.handleConditionsChange}
+          onSingleRemove={this.handleRemoveCondition}
+        />
+        <hr />
+        <button onClick={this.createRule}>create rule</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- rule_editor_prototype/src/formElements/Conditions.js
+++ rule_editor_prototype/src/formElements/Conditions.js
@@ -1,39 +1,31 @@
 import React from "react";
 import SingleCondition from "./SingleCondition";
 
-function Conditions(props)
-{
-    const ruleConditions = props.ruleConditions.slice();
-    let singleConditions = [];
-    for (let i = 0; i < ruleConditions.length; i++)
-    {
-        singleConditions.push(
-        <SingleCondition 
-            key={i}
-            conditionindex={i}
-            conditionType={props.ruleConditions[i].conditionType}
-            conditionCount={props.ruleConditions[i].conditionCount}
-            conditionObject={props.ruleConditions[i].conditionObject}
-            onChange={props.onConditionsChange}
-            onRemove={props.onSingleRemove}
-        />
-        );
-    }
-
-    return(
-        <div>
-            <h2>
-                <span style={{color: "#00ff00"}}>
-                    achievement condition(s):
-                </span>
-            </h2>
-            <button onClick={props.onAdd}>
-                add condition
-            </button>
-            <ol>
-                {singleConditions}
-            </ol>
-        </div>
+function Conditions(props) {
+  const ruleConditions = props.ruleConditions.map(condition => {
+    return condition;
+  });
+  let singleConditions = [];
+  for (let i = 0; i < ruleConditions.length; i++) {
+    singleConditions.push(
+      <SingleCondition
+        key={i}
+        conditionindex={i}
+        conditionType={props.ruleConditions[i].conditionType}
+        onChange={props.onConditionsChange}
+        onRemove={props.onSingleRemove}
+      />
     );
+  }
+
+  return (
+    <div>
+      <h2>
+        <span style={{ color: "#00ff00" }}>achievement condition(s):</span>
+      </h2>
+      <button onClick={props.onAdd}>add condition</button>
+      <ol>{singleConditions}</ol>
+    </div>
+  );
 }
 export default Conditions;
